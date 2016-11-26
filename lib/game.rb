8410f22ae85a2016281ccef6fb4881b5a9e928f6
Igor Dmitriev <igor.s.dmitriev@gmail.com>
2016-11-25 23:53:37 -0800

    Refactor Game logic.

--- lib/game.rb
+++ lib/game.rb
@@ -14,27 +14,36 @@ class Game
 
   def play_round
     shuffle_deck
-    each_player_take_five_cards
+    push_five_cards_to_each_player_from(deck)
     set_initial_card_for_stock
 
     loop do
-      players.first.play_round
-      break if players.first.dont_have_any_cards?
+      next_player.play_round(stock)
+      break if next_player.dont_have_any_cards?
       next_player!
     end
 
     calculate_points
     take_cards_from_players_to_deck
+    take_cards_from_stock_to_deck
+  end
+
+  def request_card_from_deck(player)
+    player.push_card_from(deck)
   end
 
   private
-  
+
   def game_over?
     players.any?(&:won?)
   end
 
   def next_player!
     players.rotate!
+  end
+
+  def next_player
+    players.first
   end
 
 end
